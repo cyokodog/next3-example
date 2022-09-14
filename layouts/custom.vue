3dff6e273383031aa8316beb782adf9a59bc4e6a
cyokodog <cyokodog@gmail.com>
2022-09-14 18:13:30 +0900

    add custom layout page

---
+++ layouts/custom.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="custom">
+    <NuxtLink to="/">Indexへ戻る</NuxtLink>
+    <hr/>
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+  .custom {
+    background-color:#fff
+  }
+</style>
---
+++ pages/custom.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    ここは次の処置で、カスタムレイアウトを適用したページです
+    <parts-code>definePageMeta({
+  layout: "custom"
+});</parts-code>
+
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "custom"
+});
+</script>
+
+<style scoped>
+</style>
