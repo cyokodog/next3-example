9ad172398154ef653f75d30fdade189c7731d69f
cyokodog <cyokodog@gmail.com>
2022-09-14 18:16:59 +0900

    justify custom page

---
+++ layouts/custom-layout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="wrapper">
+    カスタム
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  background-color: white;
+}
+</style>
--- pages/custom.vue
+++ pages/custom.vue
@@ -10,7 +10,7 @@
 
 <script setup>
 definePageMeta({
-  layout: "custom"
+  layout: 'custom-layout'
 });
 </script>
 
