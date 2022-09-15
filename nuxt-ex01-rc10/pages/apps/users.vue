36f1dd02a639d138e2a81c6915ede7eafdf2650d
cyokodog <cyokodog@gmail.com>
2022-09-15 16:41:20 +0900

    Web API取得データの描画をSSRにする

--- nuxt-ex01-rc10/pages/apps/users.vue
+++ nuxt-ex01-rc10/pages/apps/users.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <h2>Apps Users</h2>
-    <ClientOnly>
+    <apps-users/>
+
+    <!-- <ClientOnly>
       <apps-users/>
       <template #fallback>
-        <!-- Server 側でレンダリングされ Client 側で差し替えられる -->
         <p>loading...</p>
       </template>
-    </ClientOnly>
+    </ClientOnly> -->
   </div>
 </template>
 
